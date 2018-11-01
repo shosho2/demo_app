d6a4cb56daa8114e3112bbb83a8829602a027e47
shosho2 <shashkovq@gmail.com>
2018-11-01 17:34:05 +0300

    add helper for title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+    def full_title (page_title, base_title)
+        #base_title = "Demo app for dumm"
+        if page_title.empty? 
+            base_title
+        else 
+            "#{base_title} | #{page_title}"
+            
+        end
+    end
+
 end
